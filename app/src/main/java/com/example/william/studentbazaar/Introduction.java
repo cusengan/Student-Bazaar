1bfdd0786957c0e66f0c131f07eb38ff50feaf79
William Truong <lordvinh1@gmail.com>
2018-04-13 23:55:13 -0500

    Main screen is almost complete

--- app/src/main/java/com/example/william/studentbazaar/Introduction.java
+++ app/src/main/java/com/example/william/studentbazaar/Introduction.java
@@ -1,14 +1,10 @@
 package com.example.william.studentbazaar;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
-import com.example.william.studentbazaar.R;
 
 /**
  * Created by William on 4/13/18.
@@ -22,10 +18,10 @@ public class Introduction extends AppCompatActivity {
 //    SharePref sharePref;
     @Override
     protected void onCreate (Bundle savedInstanceState ){
-        setTheme(R.style.AppTheme);
+//        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.welcome);
+        setContentView(R.layout.introduction);
         login = (Button)findViewById(R.id.login);
         login.setOnClickListener( new View.OnClickListener(){
             @Override
--- app/src/main/java/com/example/william/studentbazaar/MainScreen.java
+++ app/src/main/java/com/example/william/studentbazaar/MainScreen.java
@@ -1,17 +1,10 @@
 package com.example.william.studentbazaar;
 
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.SyncStateContract;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 
 /**
@@ -20,18 +13,18 @@ import android.widget.Button;
 
 public class MainScreen extends AppCompatActivity  {
 
-    Button login;
-    Button register;
+    private Button mLogin;
+    private Button mRegister;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
+//        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_screen);
         Bundle intent = getIntent().getExtras();
 
-        setContentView(R.layout.welcome);
 
 
     }
